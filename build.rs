0996908837bc60ffed5a37089ce1db7e8291d5fa
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2026-09-22 16:42:49 -0300

    fix(build): package pre-built 'dist' directory to crates.io so users dont need Bun to install from crates.io

--- build.rs
+++ build.rs
@@ -2,13 +2,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use std::process::Command;
+use std::{path::Path, process::Command};
 
 fn main() {
     println!("cargo:rerun-if-changed=frontend/src");
     println!("cargo:rerun-if-changed=frontend/static");
     println!("cargo:rerun-if-changed=frontend/package.json");
     println!("cargo:rerun-if-changed=frontend/vite.config.ts");
+
+    if Path::new("dist").join("index.html").is_file() {
+        return;
+    }
 
     if std::env::var("PROFILE").unwrap() == "release" {
         let status = Command::new("bun")
